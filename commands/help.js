f739d50c3aa41207616b849302e6af67a103867e
Job <81692776+XtremeBOSSNL@users.noreply.github.com>
2023-07-30 00:47:54 +0200

    naming issue

--- commands/help.js
+++ commands/help.js
@@ -26,7 +26,7 @@ module.exports.run = async (bot, msg, args) => {
         value:`\`ew start\` Starts your journey with the bot\n` +
         `\`ew settings\` Configure the settings you want to use in the bot\n` +
         `\`ew energy\` Configure your Energy Reminder\n` +
-        `\`ew claim\` Configure your Energy Reminder\n` +
+        `\`ew claim\` Configure your Claim Reminder\n` +
         `\`ew lastclaim\` Shows how long ago you've claimed\n`
       }
     )
