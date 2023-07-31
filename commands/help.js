2fe1e8dbaccabe7661e57a804fcf2a9616629cec
Job <81692776+XtremeBOSSNL@users.noreply.github.com>
2023-07-31 19:40:07 +0200

    fix help

--- commands/help.js
+++ commands/help.js
@@ -28,7 +28,8 @@ module.exports.run = async (bot, msg, args) => {
         `\`ew energy\` Configure your Energy Reminder\n` +
         `\`ew claim\` Configure your Claim Reminder\n` +
         `\`ew raid\` Configure Your Raid Helper\n` + 
-        `\`ew lastclaim\` Shows how long ago you've claimed\n`
+        `\`ew lastclaim\` Shows how long ago you've claimed\n` +
+        `\`ew hire\` Shows your worker roll stats\n`
       }
     )
   
